888bb447b11ac1eb1122866ddb40a2a394c8a1f5
the2dge <the2dge@users.noreply.github.com>
2024-10-04 11:03:28 +0800

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,93 @@
+// Preloader //
+
+jQuery(document).ready(function($) {  
+
+
+$(window).load(function(){
+  $('#preloader').fadeOut('slow',function(){$(this).remove();});
+});
+
+});
+
+
+// Closes the sidebar menu
+$("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Closes the sidebar menu when any link inside the sidebar is clicked
+$("#sidebar-wrapper .sidebar-nav li a").click(function(e) {
+    $("#sidebar-wrapper").removeClass("active");
+});
+
+// Scrolls to the selected menu item on the page
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+});
+
+// wow
+
+new WOW().init();
+
+// Set Header Background
+
+$(window).scroll(function(){
+  var sticky = $('.sticky'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= 100) sticky.addClass('fixed');
+  else sticky.removeClass('fixed');
+});
+
+$(document).ready(function(){
+
+    // Partner slider
+        $('#partner-slider').owlCarousel({
+            loop:true,
+            margin:10,
+            autoplay: true,
+            nav: true,  // Enable navigation
+            navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'], // Add custom icons
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1440:{
+                    items:4
+                }
+            }
+        }) // end of #partner-slider
+
+});
+
+// Team overlay
+
+// $(document).ready(function(){
+//   $(".img-overlay").hover(function(){
+//     $(".overlay").toggleClass("overlay_hover");
+//   });
+
+// });
